912e95aadc120d5867cd540df0e1e9c3f677144b
Gilvan Ribeiro <g.r.almeida@live.com>
2018-07-15 21:56:11 -0300

    Creating route, view and controller for adding restaurants

---
+++ resources/views/admin/restaurants/store.blade.php
@@ -0,0 +1,17 @@
+<h1>Inserção de Restaurante</h1>
+<hr>
+<form action="">
+	<p>
+		<label for="name">Nome do Restaurante</label><br>
+		<input type="text" name="name" id="name">
+	</p>
+	<p>
+		<label for="address">Endereço</label><br>
+		<input type="text" name="address" id="address">
+	</p>
+	<p>
+		<label for="description">Fale sobre o Restaurante</label><br>
+		<textarea name="description" id="description"></textarea>
+	</p>
+	<input type="submit" value="Cadastrar">
+</form>
