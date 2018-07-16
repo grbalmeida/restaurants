7e3afdda2131fad97626226a3dd213595c7a507d
Gilvan Ribeiro <g.r.almeida@live.com>
2018-07-16 19:42:36 -0300

    Printing error messages to the user when submitting a form

--- resources/views/admin/restaurants/store.blade.php
+++ resources/views/admin/restaurants/store.blade.php
@@ -1,19 +1,27 @@
 <h1>Inserção de Restaurante</h1>
 <hr>
-{{ var_dump($errors->all()) }}
 <form action="{{ route('restaurant.store') }}" method="post">
 	{{ csrf_field() }}
 	<p>
 		<label for="name">Nome do Restaurante</label><br>
-		<input type="text" name="name" id="name">
+		<input type="text" name="name" id="name"><br>
+		@if($errors->has('name'))
+			{{ $errors->first('name') }}
+		@endif
 	</p>
 	<p>
 		<label for="address">Endereço</label><br>
-		<input type="text" name="address" id="address">
+		<input type="text" name="address" id="address"><br>
+		@if($errors->has('address'))
+			{{ $errors->first('address') }}
+		@endif
 	</p>
 	<p>
 		<label for="description">Fale sobre o Restaurante</label><br>
-		<textarea name="description" id="description"></textarea>
+		<textarea name="description" id="description"></textarea><br>
+		@if($errors->has('description'))
+			{{ $errors->first('description') }}
+		@endif
 	</p>
 	<input type="submit" value="Cadastrar">
 </form>
