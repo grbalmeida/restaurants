3bbbc1889bb36f87baefc2b2fed59e6415296611
Gilvan Ribeiro <g.r.almeida@live.com>
2018-07-16 21:35:17 -0300

    Creating authentication for user acess routes only if logged in, and styling the view index with the Bootstrap framework

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -1,26 +1,33 @@
-<h1>Restaurantes</h1>
-<a href="{{ route('restaurant.new') }}">Novo restaurante</a>
-<hr>
-<table>
-	<thead>
-		<tr>
-			<th>#</th>
-			<th>Nome</th>
-			<th>Criado em</th>
-			<th>Açõess</th>
-		</tr>
-	</thead>
-	<tbody>
-		@foreach($restaurants as $restaurant)
-			<tr>
-				<td>{{ $restaurant->id }}</td>
-				<td>{{ $restaurant->name }}</td>
-				<td>{{ $restaurant->created_at }}</td>
-				<td>
-					<a href="{{ route('restaurant.edit', ['restaurant' => $restaurant->id]) }}">Editar</a>
-					<a href="{{ route('restaurant.remove', ['id' => $restaurant->id]) }}">Excluir</a>
-				</td>
-			</tr>
-		@endforeach
-	</tbody>
-</table>
+@extends('layouts.app')
+
+@section('content')
+	
+	<div class="container">
+		<h1 class="float-left">Restaurantes</h1>
+		<a href="{{ route('restaurant.new') }}" class="float-right btn btn-success">Novo restaurante</a>
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Nome</th>
+					<th>Criado em</th>
+					<th>Açõess</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($restaurants as $restaurant)
+					<tr>
+						<td>{{ $restaurant->id }}</td>
+						<td>{{ $restaurant->name }}</td>
+						<td>{{ $restaurant->created_at }}</td>
+						<td>
+							<a href="{{ route('restaurant.edit', ['restaurant' => $restaurant->id]) }}" class="btn btn-primary">Editar</a>
+							<a href="{{ route('restaurant.remove', ['id' => $restaurant->id]) }}" class="btn btn-danger">Excluir</a>
+						</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</div>
+
+@endsection
