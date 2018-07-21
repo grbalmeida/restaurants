c53fbd10cabd16d069fd3a34a4eb537d969f403e
Gilvan Ribeiro <g.r.almeida@live.com>
2018-07-20 22:49:32 -0300

    Creating homepage for restaurants listing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,17 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <h1>Restaurantes</h1>
+    <div class="row">
+        @foreach($restaurants as $restaurant)
+            <div class="col-4">
+                <h2>{{ $restaurant->name }}</h2>
+                <p>
+                    {{ $restaurant->description }}
+                </p>
             </div>
-        </div>
+        @endforeach        
     </div>
+    {{ $restaurants->links() }}
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,7 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
+                    @auth
                     <ul class="navbar-nav mr-auto">
                         <li>
                             <a href="{{ route('restaurant.index') }}" class="nav-link">Restaurantes</a>
@@ -44,6 +45,7 @@
                             <a href="{{ route('user.index') }}" class="nav-link">Usuários</a>
                         </li>
                     </ul>
+                    @endauth
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
