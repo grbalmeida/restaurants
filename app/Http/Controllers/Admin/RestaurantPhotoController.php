99e934c48cfc87eb58e0b05be2a0bb8b9a9b78f1
Gilvan Ribeiro <g.r.almeida@live.com>
2018-07-21 08:04:13 -0300

    Adding RestaurantPhotoController

---
+++ app/Http/Controllers/Admin/RestaurantPhotoController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Restaurant;
+
+class RestaurantPhotoController extends Controller
+{
+    
+	public function index($id) {
+		$restaurant_id = $id;
+		return view('admin.restaurants.photos.index', compact('restaurant_id'));
+	}
+
+	public function save(Request $request, $id) {
+		foreach($request->file('photos') as $photo) {
+			$newName = sha1($photo->getClientOriginalName().uniqid()).'.'.$photo->getClientOriginalExtension();
+			$photo->move(public_path('images'), $newName);
+			$restaurant = Restaurant::find($id);
+			$restaurant->photos()->create([
+				'photo' => $newName
+			]);
+		}
+	}
+
+}
