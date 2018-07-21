076f249de3b375398e775b2f0e49068ec980b05e
Gilvan Ribeiro <g.r.almeida@live.com>
2018-07-21 07:59:40 -0300

    Uploading photos

--- app/RestaurantPhoto.php
+++ app/RestaurantPhoto.php
@@ -8,6 +8,10 @@ class RestaurantPhoto extends Model
 {
     protected $table = 'restaurant_photos';
 
+    protected $fillable = [
+    	'photo'
+    ];
+
     public function restaurant() {
     	return $this->belongsTo(Restaurant::class);
     }
